60ecf57116d3a3029f27c57ad63327442f8cdd7f
Arthur Mury <arthur.mury@outlook.com>
2023-08-15 00:25:44 -0300

    Bot respondendo a comandos escritos no chat

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
 import "dotenv/config";
 import { Client, IntentsBitField } from "discord.js";
-import fs from "fs";
-
-const token = process.env.DISCORD_TOKEN;
-const cryptoValue = "null";
 
 const client = new Client({
   intents: [
@@ -11,18 +7,70 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.DirectMessages
   ],
 });
 
+async function getCryptoValue(token) {
+  try {
+    const response = await fetch(`https://data.messari.io/api/v1/assets/${token}/metrics/market-data`);
+    if (!response.ok) {
+      return ({
+        status: response.status,
+        message: `[Erro] Token não esperado: ${token}`
+      });
+    }
+    const data = await response.json();
+    return (data);
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+}
+
+// debuga a execução do código no console
 client.on("ready", (clientEvent) => {
   console.log(`${clientEvent.user.tag} is ready to roll 👌`);
 });
 
-client.on("messageCreate", (message) => {
-  console.log(message.content.split()[0]);
-  //   if (message.content.split()[0] === ) {
-  //     message.reply(`Current value: ${cryptoValue}`);
-  //   }
+// ouve mensagens no chat do discord
+client.on("messageCreate", async (message) => {
+  // console.log(message);
+
+  // verifica se a mensagem foi escrita por outro bot
+  if (message.author.bot) {
+    return;
+  }
+
+  const commandString = message.content.split(" ");
+
+  if (commandString[0] === "$crypto") {
+    if (commandString.length > 1) {
+      const res = await getCryptoValue(commandString[1]);
+
+      // console.log(res);
+
+      if (res.status === 404) {
+        message.reply(res.message)
+      } else {
+        const replyContent = {
+          authorName: message.author.globalName,
+          assetName: res.data.Asset.name,
+          price: res.data.market_data.price_usd,
+        }
+
+        message.reply(
+          `
+        \`\`\`${replyContent.authorName}, aqui está o valor do ativo solicitado:\n${replyContent.assetName} (USD) $${replyContent.price}\`\`\`
+        `
+        );
+      }
+    }
+    else {
+      message.reply("[Erro] Você deve informar um Token.");
+    }
+  }
+
 });
 
+// conecta com a app do discord
 client.login(process.env.DISCORD_TOKEN);
